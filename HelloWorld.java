df976b39d71e2f41a5b2b6f3a73e77f9b000e85b
Kotturmukesh <114744108+Kotturmukesh@users.noreply.github.com>
2023-05-06 11:41:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        int a=10;
+        int b=20;
+        System.out.println(a+b); 
     }
 }
